3bdecc2510f9f2c9c268790a046d7c7df8ab8a47
Anuj Bansal <bansalanuj9@gmail.com>
2020-03-14 12:13:32 +0530

    Added socket.io

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 4000;
+const socketio = require('socket.io');
 
 const server = http.createServer(app);
+const io = socketio(server);
 
 server.listen(port, function() {
     console.log('App listening on port 4000');
 })
+
+io.on('connection', function(socket) {
+    console.log(socket.id)
+});
