9c12e63ceb96a31426bc70730b6347a2a19f6291
Anuj Bansal <bansalanuj9@gmail.com>
2020-03-14 15:28:29 +0530

    Added socket room

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ const socketio = require('socket.io');
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', function (socket) {
+    socket.on('join', function (data) {    
+     socket.join(data.id);
+     console.log("Joined: "+ data.id);
+     
+   });
+ });
+
 server.listen(port, function() {
     console.log('App listening on port 4000');
 })
 
-io.on('connection', function(socket) {
-    console.log(socket.id)
-});
