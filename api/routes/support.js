3bd9938919b9d11ca23176eb6f80654448d4bcfe
Yuvraj Midha <inventive8983@gmail.com>
2020-03-29 23:55:20 +0530

    support dashboard beta

--- api/routes/support.js
+++ api/routes/support.js
@@ -7,20 +7,31 @@ const Issues = require('../models/reportissue')
 const auth = require('../../config/auth')
 const User = require('../models/user')
 const token = 'inh#$ygf^&tejd457867bct5we64r//@342?SDGER34fyt5d5t@'
+const sendEmail = require('../../scripts/sendEmail')
 
-//for checking the current status
-router.get('/projects', (req,res)=>
+//Render Views
+router.get('/', (req,res)=>
 {
-
-    const current= req.query.currentStatus;
-
-    Project.find({currentStatus:current})
+    var freelancers = ''
+    Freelancer.find().then(result => {
+        freelancers = result.length
+    })
+    
+    Project.find({})
     .then(result=>{
-        res.status(200).json({
-            type:"success",
-            message:"Status sent successfully",
-            data: result
+        var earning = 0
+        result.forEach(project => {
+            earning += project.pay.paidAmount
         })
+
+        var analytics = {
+            projects: result.length,
+            working: result.filter(project => project.currentStatus === 'Working').length,
+            earnings: earning,
+            freelancers: freelancers
+        }
+        console.log(analytics)
+        res.render('index', {projects: result, analytics: analytics})
     })
     .catch(err=>
         {
@@ -33,16 +44,253 @@ router.get('/projects', (req,res)=>
         })
 
 })
+
+//for checking the current status
+router.get('/projects', (req,res)=>
+{
+    
+    Project.find({})
+    .then(result=>{
+
+        let months = []
+        let red = []
+        let yellow = []
+        let green = []
+
+        let services = []
+        let servicesCount = []
+
+        result.forEach(project => {
+            let month = project.dateCreated.toString().split(' ')[1] + ' ' + project.dateCreated.toString().split(' ')[3]
+            if(months.includes(month) === false){
+                months.push(month)
+            }
+            if(project.serviceMode === 'Green' || project.serviceMode === 'green'){
+                if(green[months.indexOf(month)]) green[months.indexOf(month)]++
+                else green[months.indexOf(month)] = 1
+                yellow[months.indexOf(month)] = 0
+                red[months.indexOf(month)] = 0
+            }
+            if(project.serviceMode === 'Yellow' || project.serviceMode === 'yellow'){
+                if(yellow[months.indexOf(month)]) yellow[months.indexOf(month)]++
+                else yellow[months.indexOf(month)] = 1
+                green[months.indexOf(month)] = 0
+                red[months.indexOf(month)] = 0
+            }
+            if(project.serviceMode === 'Red' || project.serviceMode === 'red'){
+                if(red[months.indexOf(month)]) red[months.indexOf(month)]++
+                else red[months.indexOf(month)] = 1
+                yellow[months.indexOf(month)] = 0
+                green[months.indexOf(month)] = 0
+            }
+
+            project.services.forEach(element => {
+                services.push(element.serviceName)
+            })
+            
+        })
+
+        servicesCount[0] = services.filter(element => element === 'Logo').length
+        servicesCount[1] = services.filter(element => element === 'Posters').length
+        servicesCount[2] = services.filter(element => element === 'ID Cards').length
+        servicesCount[3] = services.filter(element => element === 'Business Cards').length
+        servicesCount[4] = services.filter(element => element === 'Pamphlets').length
+        servicesCount[5] = services.filter(element => element === 'Letterhead').length
+        servicesCount[6] = services.filter(element => element === 'Brochure').length
+        servicesCount[7] = services.filter(element => element === 'Certificate').length
+        servicesCount[8] = services.filter(element => element === 'Billboard').length
+        servicesCount[9] = services.filter(element => element === 'Banners').length
+
+        console.log(servicesCount)
+        var analytics = {
+            projects: result.length,
+            working: result.filter(project => project.currentStatus === 'Working').length,
+            completed: result.filter(project => project.currentStatus === 'Completed').length,
+            declined: result.filter(project => project.declineStatus === true).length,
+            monthArray: months,
+            green: green,
+            red: red,
+            yellow: yellow,
+            services: servicesCount
+        }
+        console.log(analytics)
+        res.render('projects', {projects: result, analytics: analytics})
+    })
+    .catch(err=>
+        {
+
+            res.status(400).json({
+                type:"error",
+                message:"there is an error",
+                data:err
+            })
+        })
+
+})
+
+//for filtering the freelancers
+
+router.get('/freelancers', (req, res)=>
+{    
+
+ 
+    Freelancer.find({})
+    .then(result=>{
+        res.status(200).render('freelancers', {freelancers: result})
+     })
+    .catch(err=>
+        {
+            res.send(400).json({
+                type:"failure",
+                err:err
+            })
+        })
+
+})
+
+router.get('/clients', (req, res) => {
+
+    User.find({})
+    .then(result=>{
+        res.status(200).render('clients', {clients: result})
+     })
+    .catch(err=>
+        {
+            res.send(400).json({
+                type:"failure",
+                err:err
+            })
+        })
+})
+
+router.get('/addfreelancers', (req, res) => {
+
+    Freelancer.find({}).then(result => {
+        res.render('taskviews/addfreelancers', {totalFreelancers: result.length})
+    }).catch(err => {
+        console.log(err)
+    })
+    
+})
+
+router.get('/assignfreelancer/:projectId', (req, res) => {
+
+    Project.findOne({_id: req.params.projectId}).then(project => {
+        res.render('taskviews/assign', {
+            projectId: project._id,
+            projectName: project.name
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+
+    
+})
+
+router.get('/changebudget/:projectId', (req, res) => {
+
+    Project.findOne({_id: req.params.projectId}).then(project => {
+        res.render('taskviews/budget', {
+            projectId: project._id,
+            projectName: project.name,
+            budget: project.budget
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+
+})
+
+router.get('/changestatus/:projectId', (req, res) => {
+
+    Project.findOne({_id: req.params.projectId}).then(project => {
+        res.render('taskviews/changestatus', {
+            projectId: project._id,
+            projectName: project.name,
+            projectStatus: project.currentStatus
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+
+    
+})
+
+router.get('/changepayment/:projectId', (req, res) => {
+
+    Project.findOne({_id: req.params.projectId}).then(project => {
+        res.render('taskviews/payment', {
+            projectId: project._id,
+            projectName: project.name,
+            currentPayment: project.pay.totalAmount
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+
+})
+
+router.get('/sendcustomemail', (req, res) => {
+           res.render('taskviews/sendcustomemail',)
+})
+
+router.get('/sendemail/:id', (req, res) => {
+
+    User.findOne({_id: req.params.id}).then(user => {
+        res.render('taskviews/sendmail', {
+            email: user.email,
+            name: user.name
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+
+})
+
+
+router.get('/freelancer/sendemail/:id', (req, res) => {
+
+    Freelancer.findOne({_id: req.params.id}).then(freelancer => {
+        res.render('taskviews/sendmail', {
+            email: freelancer.email,
+            name: freelancer.name
+        })
+    }).catch(err => {
+        console.log(err)
+    })
+
+})
+//isssues get rest api based on solved and unsolved
+
+router.get('/issues',(req,res)=>
+{
+    Issues.find({})
+    .then(result=>
+        {   
+            console.log(result)
+            res.status(200).render('issues', {issues: result})
+        })
+    .catch(err=>
+        {
+            res.status(400).json({
+                type:"Failure",
+                data:err
+              })
+        })
+})
+
+
 //assign freelancer to a project
 
-router.patch('/freelancer/assign/:projectId',(req, res)=> {
+router.post('/freelancer/assign/:projectId',(req, res)=> {
 
 
     const id =req.body.id
     const projectId = req.params.projectId
     Project.updateOne({_id:projectId},{
                         $set: { 
-                            freelancerId:id,currentStatus:"Freelancer assigned"
+                            freelancerId:id, 
+                            currentStatus:"Freelancer Assigned"
                         }
                     })
                     .then(result=>{
@@ -60,61 +308,39 @@ router.patch('/freelancer/assign/:projectId',(req, res)=> {
                     })
     })
 
-
-
-//for filtering the freelancers
-
-router.get('/freelancers/', (req, res)=>
-{    
-
-   let quer ={}
-
-    for(var key in req.query){ 
-        console.log(typeof req.query[key]) 
-        if(typeof req.query[key] === 'object'){
-            quer[key] = req.query[key].sort();
-         } 
-
-        else quer[key] = req.query[key];
-
-    } 
-  
-    Freelancer.find(quer)
-
-    .then(result=>{
-        res.status(200).json({
-            type:"success",
-            message:"Data is Displaying",
-            data:result
-        })
-     })
-    .catch(err=>
-        {
-            res.send(400).json({
-                type:"failure",
-                err:err
-            })
-        })
-
-})
-
-
 //remove freelancer from the project
 
-router.patch('/freelancer/remove/:projectId',(req, res)=>
+router.get('/freelancer/remove/:projectId',(req, res)=>
 {   
 
-    const id =req.body.id
+    var freelancer = ''
     const projectId = req.params.projectId
-    Project.updateOne({_id:projectId},{$set:{freelancerId:null,currentStatus:"Freelancer not assigned",decFreelancerId:id}})
-    .then(result=>
-        {
-            res.status(200).json({
-                type:"Success",
-                message:"Freelancer removed successfully",
-                data:result
+
+    Project.findOne({_id: projectId})
+    .then(result => {
+        freelancer = result.freelancerId
+
+        Project.updateOne({_id:projectId},{$set:{
+            freelancerId:null,
+            currentStatus:"Assigning Freelancer", 
+            decFreelancerId:freelancer}})
+        .then(result=>
+            {
+                res.status(200).json({
+                    type:"success",
+                    message:"Freelancer removed successfully",
+                    data:result
+                })
             })
-        })
+        .catch(err=>
+            {
+                res.send(400).json({
+                    type:"failure",
+                    err:err
+                })
+            })
+
+    })
     .catch(err=>
         {
             res.send(400).json({
@@ -122,17 +348,38 @@ router.patch('/freelancer/remove/:projectId',(req, res)=>
                 err:err
             })
         })
+    
 })
 
 
 //undecline the project 
 
-router.patch('/undecline/:projectId',(req, res) =>
+router.post('/changestatus/:projectId',(req, res) =>
 
 {   
 
+    var status = ''
+    var decline = false
+
+    if(req.body.status === 'Declined'){
+        Project.find({_id: req.params.projectId}).then(result => {
+            status = result.currentStatus
+        }).catch(err => {
+            res.send(err)
+        })
+
+        var decline = true
+    }
+    else{
+        status = req.body.status
+    }
+
     const id = req.params.projectId
-    Project.updateOne({_id:id},{$set:{declineStatus:false}})
+    Project.updateOne({_id:id},{$set:{
+        currentStatus: status,
+        declineStatus:false
+        }
+    })
     .then(result=>
         {
             res.status(200).json({
@@ -174,7 +421,7 @@ router.post('/issues/add', auth, (req,res)=>
                         description: req.body.description,
                         clientId:clientId,
                         projectName:projectName,
-                        clientContct:clientContact,
+                        clientContact:clientContact,
                         clientName:clientName
                         })
                         issue.save().then(result =>
@@ -207,47 +454,108 @@ router.post('/issues/add', auth, (req,res)=>
             
 })
 
-//isssues get rest api based on solved and unsolved
 
-router.get('/issues/get',(req,res)=>
-{
-    const status = req.query.status
-    Issues.find({status:status})
-    .then(result=>
-        {
-            res.status(200).json({
-                type:"Success",
-                message:"Issues are Displaying",
-                data:result
-            })
+
+router.get('/updateIssue/:reportId', (req, res) => {
+
+    Issues.findOne({_id: req.params.reportId}).then(result => {
+
+        let status = result.status === 'Solved' ? 'Unsloved' : 'Solved'
+
+        Issues.updateOne({_id: req.params.reportId}, {
+            $set: {
+                "status": status
+            }
+        }).then(result => {
+            res.status(200).redirect("/api/support/issues")
         })
-    .catch(err=>
-        {
-            res.status(400).json({
-                type:"Failure",
-                data:err
-              })
+        .catch(err => {
+            console.log(err)
+            res.status(400)
         })
+    }).catch(err => {
+        console.log(err)
+        res.status(400)
+    })
+
+    
 })
 
-router.get('/updateIssue/:status/:reportId', (req, res) => {
-    Issues.findOneAndUpdate({_id: req.params.reportId}, {
-        $set: {
-            "status": req.params.status
+//Send Email 
+router.post('/sendmail', (req, res) => {
+
+    const to = req.body.mailId
+    const subject = req.body.subject
+    const name = req.body.name
+    const body = req.body.body
+    const button = {
+        text: req.body.btnText,
+        link: req.body.btnLink
+    }
+
+    sendEmail(to, subject, body, name, button, (success, message) => {
+        if(success){
+            res.status(200).send(`Mail sent seccessfuly to ${to}`)
         }
-    }).then(result => {
+        else{
+            res.status(400).send(message)
+        }
+    })
+
+})
+
+router.get('/delete/:role/:id', (req, res) => {
+
+    var Model = req.params.role
+
+    User.deleteOne({_id: req.params.id}).then(result => {
         res.status(200).json({
             type: 'success',
-            message: 'Updated Successfully',
+            message: Model + ' deleted successfully.',
             data: result
         })
     })
     .catch(err => {
-        console.log(err)
-        res.status(400)
+        res.status(200).json({
+            type: 'error',
+            error: err
+        })
     })
+
 })
 
+router.post('/budget/:id', (req, res) => {
 
+    Project.updateOne({_id: req.params.id},{
+        $set: {
+            budget: req.body.budget
+        }
+    }).then(result => {
+        res.status(200).send("Updated Budget Successfully")
+    }).catch(err => {
+        res.status(400).send(err)
+    })
+
+})
+
+router.post('/payment/:id', (req, res) => {
+    
+    const payment = parseInt(req.body.payment)
+    console.log(payment)
+    Project.updateOne({_id: req.params.id},{
+        $set: {
+            "pay.totalAmount" : payment
+        }
+    }).then(result => {
+        res.status(200).json({
+            type: 'success',
+            message: "Updated Amount Successfully",
+            data: result
+        })
+    }).catch(err => {
+        res.status(400).send(err)
+    })
+
+})
 
 module.exports= router;
